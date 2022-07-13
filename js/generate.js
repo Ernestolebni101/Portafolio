234629642c0dbddfb2adb5f8ac016c033ea42310
emiranda <ernestolebni123@gmail.com>
2022-07-12 23:29:42 -0600

    mn

---
+++ js/generate.js
@@ -0,0 +1,69 @@
+
+const technologys = [{
+    tec: "Javascript & TypesCript",
+    proggress: "90%"
+},
+{
+    tec: "SQL",
+    proggress: "95%"
+},
+{
+    tec: "NOSQL",
+    proggress: "95%"
+},
+{
+    tec: "C#",
+    proggress: "100%"
+},
+{
+    tec: "C++",
+    proggress: "70%"
+},
+{
+    tec: "R",
+    proggress: "65%"
+},
+{
+    tec: "Python",
+    proggress: "95%"
+},
+{
+    tec: "Html & Css",
+    proggress: "95%"
+}];
+
+
+const frameworks = [
+    {
+        framework: "Express",
+        proggress: "95%"
+    },
+    {
+        framework: "Nesjts",
+        proggress: "90%"
+    },
+    {
+        framework: "Asp.Net Core & Framework",
+        proggress: "95%"
+    },
+    {
+        framework: "Flask",
+        proggress: "80%"
+    },
+    {
+        framework: "FastApi",
+        proggress: "70%"
+    },
+    {
+        framework: "Pytorch",
+        proggress: "70%"
+    },
+    {
+        framework: "Angular",
+        proggress: "95%"
+    }];
+
+
+//const techSkill = document.getElementById('techSkill');
+
+technologys.forEach(t => console.log(t));
